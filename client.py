18b6bb2b8b9bfcf49e55a6067c4a05bc8d9da9f6
Maroof Ahmed <96487710+M4700F@users.noreply.github.com>
2025-10-17 08:52:20 +0600

    Fix some indentation

--- client.py
+++ client.py
@@ -17,8 +17,8 @@ model.compile(
 
 
 
-# parameters are the weights that server sends to the client
-# Config are the dictionary of instruction like number of epoch, learning rate, etc hyperparameters
+# parameters are the weights that the server sends to the client
+# Config are the dictionary of instruction like number of epochs, learning rate, etc. hyperparameters
 
 class FlowerClient(fl.client.NumPyClient):
   def get_parameters(self, config):
@@ -34,4 +34,7 @@ class FlowerClient(fl.client.NumPyClient):
     loss, accuracy = model.evaluate(x_test, y_test)
     return loss, len(x_test), {"accuracy": accuracy}
 
-fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=FlowerClient())
+fl.client.start_numpy_client(
+    server_address="127.0.0.1:8080",
+    client=FlowerClient()
+)
